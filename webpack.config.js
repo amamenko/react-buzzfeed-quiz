2aea8f0fd8641d128df83d29d4c13432ed5b7356
amamenko <amamenko@hotmail.com>
2021-02-19 17:52:30 -0500

    Updated Proxima Nova Black font file and associated CSS

--- webpack.config.js
+++ webpack.config.js
@@ -14,11 +14,7 @@ module.exports = {
         test: /\.js?$/,
         exclude: /(node_modules)/,
         use: "babel-loader",
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
+      }
     ],
   },
   resolve: {
